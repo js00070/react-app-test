7b00121ee09c3e228a1dc1fa4ee491e8c43f3007
liyuqian <liyuqian@bytedance.com>
2019-01-24 13:53:02 +0800

    Updates

---
+++ precache-manifest.d3a375976867cb2025034fe91ceca752.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "7fb6029d23c0665e6afb",
+    "url": "/react-app-test/static/js/runtime~main.7fb6029d.js"
+  },
+  {
+    "revision": "14836fa6c38b35a4d228",
+    "url": "/react-app-test/static/js/main.14836fa6.chunk.js"
+  },
+  {
+    "revision": "d687df5dd64bedbd2740",
+    "url": "/react-app-test/static/js/1.d687df5d.chunk.js"
+  },
+  {
+    "revision": "14836fa6c38b35a4d228",
+    "url": "/react-app-test/static/css/main.0a9d6a28.chunk.css"
+  },
+  {
+    "revision": "d687df5dd64bedbd2740",
+    "url": "/react-app-test/static/css/1.e1112897.chunk.css"
+  },
+  {
+    "revision": "0590aa6b0ff232218901dfd9581850d8",
+    "url": "/react-app-test/index.html"
+  }
+];
